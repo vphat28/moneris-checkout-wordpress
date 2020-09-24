3e78569df0b10b7982d5ce4ec1268f3bb495beec
Xavi Nguyen <vphat28@gmail.com>
2020-09-24 15:52:03 +0700

    Update

--- classes/Gateway.php
+++ classes/Gateway.php
@@ -168,6 +168,7 @@ class Gateway extends \WC_Payment_Gateway {
 		$helper->log( 'hash ' . $quoteId );
 		$helper->log( 'order_id moneris ' . $receipt['response']['request']['order_no'] );
 		$helper->log( 'receipt for order id ' . $order_id . ' _ ' . json_encode( $receipt ) );
+		$helper->log( 'approve id ' . $receipt["response"]["receipt"]["cc"]["transaction_code"] );
 
 		if ( $receipt["response"]["receipt"]["cc"]["transaction_code"] === 'null' ) {
             wc_add_notice( __( 'Payment error', 'woocommerce' ), 'error' );
